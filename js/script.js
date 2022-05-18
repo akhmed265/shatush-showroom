5bf2baee7ad8ea550ccaf7052693a45933d3f804
akhmed265 <malachiyev_akhmed@mail.ru>
2022-05-18 22:24:25 +0300

    adaptive

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-const brandSlider = jQuery('.brand-detail__slider');
+// Настройка слайдера
+if (window.jQuery) {
+  const brandSlider = jQuery('.brand-detail__slider');
 
-jQuery(document).ready(function() {
-  jQuery(brandSlider).lightSlider({
-      item:1,
-      controls: false,
-      auto: true,
-      loop: true,
+  jQuery(document).ready(function() {
+    jQuery(brandSlider).lightSlider({
+        item:1,
+        controls: false,
+        auto: true,
+        loop: true,
+    });
   });
+}
+//  Настройка слайдера End
+
+// menu
+const menuToggleBtn = document.querySelector('.header-nav-toggle');
+const mainMenu = document.querySelector('.header-nav');
+
+menuToggleBtn.addEventListener('click', function() {
+  mainMenu.classList.toggle('header-nav_show');
 });
+// menu end
